714974fd46754787f1882ee1f81a0d4bcdd02829
Anna Waczyńska <waczynska.a@gmail.com>
2023-12-06 18:44:50 +0100

    First

---
+++ src/main/java/com/example/springsecurity/student/model/Student.java
@@ -0,0 +1,19 @@
+package com.example.springsecurity.student.model;
+
+public class Student {
+    private final Integer studentId;
+    private final String StudentName;
+
+    public Student(Integer studentId, String studentName) {
+        this.studentId = studentId;
+        StudentName = studentName;
+    }
+
+    public Integer getStudentId() {
+        return studentId;
+    }
+
+    public String getStudentName() {
+        return StudentName;
+    }
+}
