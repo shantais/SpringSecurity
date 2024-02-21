87eacd7736bcdf13ae178264ced70a05cbc3fb01
shantais <shantais90@gmail.com>
2024-02-21 19:46:54 +0100

    Basic-Auth 06.12 index.html and whitelist in ApplicationSecurityConfig

--- src/main/java/com/sda/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/sda/springsecurity/security/ApplicationSecurityConfig.java
@@ -8,9 +8,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity // adnotacja określająca że klasa będzie zawierała konfogurację dla Security
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+    // TODO: ogarnąć jak się w security robi WHITELIST w oddzielnej metodzie
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests() // deklaracja, że rządania mają być autoryzowane
+                .antMatchers("/", "/index") // WHITELIST, czyli to co nie wpływa na authentykację
+                .permitAll()
                 .anyRequest() // jakie rządania?: kazde
                 .authenticated() // musi przejść authentykację (user i password)
                 .and()
